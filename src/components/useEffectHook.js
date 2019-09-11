523a151578c6d1ad01453598a9a84dc446db5120
N ADITYA SANDEEP <sandeep.mohan@anywhere.co>
2019-09-11 22:07:35 +0530

    added useeffect hook

--- src/components/useEffectHook.js
+++ src/components/useEffectHook.js
@@ -1,10 +1,22 @@
 /**
+ * OVERVIEW
+ * - CONDITIONAL REDERNING
+ * - USE ONLY ONCE (COMPONENTDIDMOUNT)
+ * - WITH CLEANUP (COMPONENTWILLUNMOUNT)
+ */
+
+/**
  * useEffect hook is a close replacement of
  * componentdidUpdate, DidMount, WillUnmount
  *
- * MEMO
+ * MEMO or CONDITIONALLY run the effects
  * useEffect(() => {
- * }, [<value>, <value>, ...])
+ * Note: If the value 1 changes only the useEffect will run
+ * }, [<value1>])
+ *
+ * * useEffect(() => {
+ * Note: If both value 1 & 2 changes only the useEffect will run
+ * }, [<value1, value 2>])
  *
  * componentWillUnMount
  * useEffect(() => {
@@ -14,4 +26,51 @@
  * }
  * })
  *
+ */
+import React, { useState, useEffect } from "react";
+function HooksButton() {
+  // count : the value
+  // setCount : this.setState()
+  /**
+   * A state value can be a
+   * @string
+   * @object
+   * @bool
+   * @array
+   */
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    console.log(`You clicked the button ${count} times`);
+  }, [count]);
+  return (
+    <div>
+      <button onClick={() => setCount(prevState => prevState + 1)}>
+        Count {count}{" "}
+      </button>
+    </div>
+  );
+}
+
+export default HooksButton;
+
+/**
+ *  * - USE ONLY ONCE (COMPONENTDIDMOUNT)
+ * To achive only once to use the useEffect
+ * The console log only print once it wont update after clicking
+ * This can be used in the adding event listeners
+ *   useEffect(() => {
+    console.log(`You clicked the button ${count} times`);
+  }, []); // empty array []
+ */
+
+/**
+ *  * - MIMIC THE (COMPONENTWILLUNMOUNT)
+ * To achive only once to use the useEffect
+ * The console log only print once it wont update after clicking
+ * This can be used in the adding event listeners
+ *   useEffect(() => {
+    return () => {
+      console.log(`component is unmounted`);
+    }
+  }, []); // empty array []
  */
--- src/index.js
+++ src/index.js
@@ -3,12 +3,17 @@ import ReactDOM from "react-dom";
 
 import "./styles.css";
 import HackButton from "./components/useStateHook";
+import UseEffectButtion from "./components/useEffectHook";
+
 function App() {
   return (
     <div className="App">
       <div>
         <h1>useState Hook</h1>
         <HackButton />
+        <h1>useEffect Hook </h1>
+        <p>(check console for logs)</p>
+        <UseEffectButtion />
       </div>
     </div>
   );
